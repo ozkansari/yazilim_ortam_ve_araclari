e3dcba2565f8bbd2efa9626c541137bd6f77f7c1
ozkansari <ozkansari@gmail.com>
2020-12-02 21:22:29 +0300

    spring ornek

---
+++ ders7/src/test/java/tr/edu/medipol/yazilimaraclari/ders7/YazilimAraclariDers7ApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazilimaraclari.ders7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YazilimAraclariDers7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
