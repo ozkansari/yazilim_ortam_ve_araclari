5db92ed28c28511dd57eec7dcab2438f33f67467
ozkansari <ozkansari@gmail.com>
2021-03-25 20:05:34 +0300

    Ders 4 maven ornek proje

---
+++ Ders4MavenProje/src/main/java/tr/edu/medipol/yazortarac/MetinIslemleri.java
@@ -0,0 +1,17 @@
+package tr.edu.medipol.yazortarac;
+
+import org.apache.commons.text.WordUtils;
+
+public class MetinIslemleri {
+
+	public static void main(String[] args) {
+		
+		String metin = "Besiktas Jimnastik Kulubu";
+		
+		String sonuc = WordUtils.initials(metin);
+		
+		System.out.println(metin + " -> " + sonuc);
+		
+	}
+
+}
